66ba0aded0fe04a4a994cb370c44869c78f96de0
Aya <alcantarahuerta128@gmail.com>
2024-11-06 08:38:43 -0800

    Disenio
    inplementando el disenio de la pantalla principal

--- principal.php
+++ principal.php
@@ -1,14 +1,14 @@
 <?php 
     include "includes/headerlogin.php";
 ?>
-<br>
 <body>
-    <div>
-        <a href="marcados.php" class="btn btn-marcado">Mark attendance</a>
+    <div class="btn-container">
+        <a href="marcados.php" class="btn btn-marcado" aria-label="Mark Attendance">
+            <i class="fas fa-check-circle"></i> Mark Attendance
+        </a>
+        <a href="Session/login.php" class="btn btn-portal" aria-label="Enter Employment Portal">
+            <i class="fas fa-door-open"></i> Enter Employment Portal
+        </a>
     </div>
-
-    <div>
-        <a href="Session/login.php" class="btn btn-portal">Enter Employ Portal</a>
-    </div>
-
 </body>
+</html>
