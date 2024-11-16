0b4222321f93438a5948c26a62bbcf111cc493ef
Miguellr01 <0323106022@ut-tijuana.edu.mx>
2024-11-15 19:43:05 -0800

    Cambios en principal

--- principal.php
+++ principal.php
@@ -2,16 +2,28 @@
     include "includes/headerlogin.php";
 ?>
 <body>
-    <div class="btn-container">
-    <h2>Human resources</h2>
-    <h3>Welcome to the main menu</h3>
-    <p>Select one of the following options</p>
-        <a href="marcados.php" class="btn btn-marcado" aria-label="Mark Attendance">
-            <i class="fas fa-check-circle"></i> Mark Attendance
-        </a>
-        <a href="Session/login.php" class="btn btn-portal" aria-label="Enter Employment Portal">
-            <i class="fas fa-door-open"></i> Enter Employment Portal
-        </a>
+    <div class="banner">
+        <div class="navbar">
+            <img src="../Principal/img/logo.png" alt="" class="logo">
+            <!--<ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>-->
+        </div>
+
+        <div class="content">
+            <h1>Human Resources</h1>
+            <h2>Welcome to the main menu</h2>
+            <p>elect one of the following options</p>
+            <div>
+            <button type="button" onclick="window.location.href='marcados.php'"><span></span>Mark Attendance</button>
+            <button type="button" onclick="window.location.href='Session/login.php'"><span></span>Enter Employment Portal</button>
+        </div>
+        </div>
+
     </div>
+
 </body>
 </html>
