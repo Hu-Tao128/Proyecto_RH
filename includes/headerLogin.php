68a94b55745ee97099c20d7320660fe2896ceae1
Aya <alcantarahuerta128@gmail.com>
2024-11-16 23:18:43 -0800

    ok

--- includes/headerLogin.php
+++ includes/headerLogin.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="css/login.css">
+    <meta name=”viewport” content="width=device-width, intial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css/principal.css" />
+    <link rel="stylesheet" type="text/css" href="css/header.css" />
 </head>
