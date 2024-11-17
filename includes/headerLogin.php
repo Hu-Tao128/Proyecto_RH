82f27905e40a368d5a31abfa6efce42944bee19f
Miguellr01 <0323106022@ut-tijuana.edu.mx>
2024-11-17 14:23:27 -0800

    Update headerLogin.php

--- includes/headerLogin.php
+++ includes/headerLogin.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta name=”viewport” content="width=device-width, intial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="css/principal.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css" />
     <link rel="stylesheet" type="text/css" href="css/header.css" />
 </head>
