785b25bab0d9ceb7cda34c6a19c1ad9b0474a578
LuisAlberto2304 <0323105921@ut-tijuana.edu.mx>
2024-11-20 17:49:58 -0800

    boton

--- changeInformation.php
+++ changeInformation.php
@@ -1,45 +1,42 @@
-<!-- 
+ 
 <?php include "includes/header.php" ?>
 <section>
     <div>
+        <h3>Change the information</h3>
         <form action="" class="formPage">
             <fieldset>
+                <br>
                 <div>
-                    <input type="text" name="number" id="number">
-                </div>
-                <div>
-                    <input type="text" name="name" id="name">
-                </div>
-                <div>
-                    <input type="text" name="lastName" id="lastName">
-                </div>
-                <div>
+                    <label for="email">Email</label>
                     <input type="text" name="email" id="email">
                 </div>
+                <br>
                 <div>
-                    <input type="text" name="gender" id="gender">
-                </div>
-                <div>
-                    <input type="text" name="age" id="age">
-                </div>
-                <div>
+                    <label for="phoneNumber">Phone Number</label>
                     <input type="text" name="phoneNumber" id="phoneNumber">
                 </div>
+                <br>
                 <div>
+                    <label for="password">Password</label>
                     <input type="text" name="password" id="password">
                 </div>
+                <br>
                 <div>
                     <button type="submit">Change data</button>
                 </div>
                 <div>
-                    <button>Cancel</button>
+                   
                 </div>
             </fieldset>
+            
         </form>
+        <center>
+            <a href="information.php">
+                <button class="loginButton">Cancel</button>
+            </a>
+        </center>              
     </div>
 </section>
 
 <?php include "includes/footer.php" ?>
--->
-
 
