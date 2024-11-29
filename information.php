b51dd4d7f2dbd21f8910caa19fc8c7ede191ba86
Aya <alcantarahuerta128@gmail.com>
2024-11-29 11:08:52 -0800

    asfa

--- information.php
+++ information.php
@@ -10,6 +10,11 @@ $image = $info['image'];
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>  <!-- Cargar Chart.js -->
+</head>
+
 <section class="position"><br>
     <div class="container">
         <h1 class="tittlePerfil">Profile Settings about Employe <?php echo $IDUsuario?></h1>
