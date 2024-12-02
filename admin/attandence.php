c22717e5a59119d1ef2447be8e3b8e37eb6854c9
Miguellr01 <0323106022@ut-tijuana.edu.mx>
2024-12-01 22:04:14 -0800

    attendece Supervisor

--- admin/attandence.php
+++ admin/attandence.php
@@ -5,42 +5,52 @@ require_once "functionsAdmin.php";
 
 $attandance = getAttendance($IDUsuario);
 ?>
-<section>
-    <center>
-        <div class="questions">
-        <h2>Table for the attandence</h2>
-        <p>In this section you can see the list of clock-in and clock-out of the employees you supervise. Here you can only delete assists made.</p>
-        </div>
-    </center>
-<br>
-    <div class="scroll">
-        <table border="1" class="tableAdmin">
-            <tr>
-                <th>number</th>
-                <th>Start Date</th>
-                <th>End Date</th>
-                <th>Employee</th>
-                <th>Options</th>
-            </tr>
-            <?php foreach ($attandance as $renglon) { ?>
-            <tr>
-                <td><?= $renglon['number']?></td>
-                <td><?= $renglon['startDate']?></td>
-                <?php
-                    if ($renglon['endDate'] == '' || $renglon['endDate'] == null) {?>
-                        <td>Working</td><?php
-                    }else{?>
-                        <td><?= $renglon['endDate']?></td><?php
-                    }?>
-                <?php 
-                $employ = $renglon['employee'];
-                $name = firstname($employ);
-                $lastname = lastname($employ);?>
-                <td><?php echo $name." ".$lastname;?></td>
-                <td><a href="deleteAttandance.php?id=<?= $renglon['number']?>&action=delete" class="action-delete">Delete</a></td>
-            </tr><?php
-            } ?>
+<section class="container mt-5">
+    <div class="text-center mb-4">
+        <h2>Table for the Attendance</h2>
+        <p>In this section, you can see the list of clock-ins and clock-outs of the employees you supervise. Here you can only delete assists made.</p>
+    </div>
+
+    <!-- Contenedor con scroll -->
+    <div class="table-responsive" style="max-height: 400px; overflow-y: auto;">
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="table-dark">
+                <tr>
+                    <th>Number</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Employee</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($attandance as $renglon) { ?>
+                <tr>
+                    <td><?= $renglon['number'] ?></td>
+                    <td><?= $renglon['startDate'] ?></td>
+                    <td>
+                        <?php if (empty($renglon['endDate'])) { ?>
+                            <span class="text-warning">Working</span>
+                        <?php } else { ?>
+                            <?= $renglon['endDate'] ?>
+                        <?php } ?>
+                    </td>
+                    <?php 
+                    $employ = $renglon['employee'];
+                    $name = firstname($employ);
+                    $lastname = lastname($employ); ?>
+                    <td><?= $name . " " . $lastname ?></td>
+                    <td>
+                        <a href="deleteAttandance.php?id=<?= $renglon['number'] ?>&action=delete" 
+                           class="btn btn-danger btn-sm">
+                           Delete
+                        </a>
+                    </td>
+                </tr>
+                <?php } ?>
+            </tbody>
         </table>
     </div>
 </section>
+
 <?php include "../includes/footer.php" ?>
