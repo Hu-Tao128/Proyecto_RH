12b995788d777cdd4ed9693e025163ae9f027fad
Miguellr01 <0323106022@ut-tijuana.edu.mx>
2024-12-01 18:15:41 -0800

    Diseno admin/rating

--- admin/rating.php
+++ admin/rating.php
@@ -1,4 +1,4 @@
-<?php include "../includes/headerAdmin.php";
+<?php include "../includes/headerSupervisor.php";
 require_once "../includes/config/MySQL_ConexionDB.php";
 require_once "functionsAdmin.php"; 
 require_once "../functions.php"; 
@@ -6,75 +6,82 @@ require_once "../functions.php";
 $rating = showRatings($IDUsuario);
 ?>
 
-<section>
-    <center>
-        <div class="questions">
+<section class="container mt-5">
+    <div class="text-center mb-4">
         <h2>Table for the rating</h2>
-        <p>In this section you can see the scores you have made on the employees you supervise, you can modify the reports you have made and also delete them.
-        <br><br>
-At the bottom you have a form where you can make employee score reports.</p>
-        </div>
-    </center>
-        <br>
-    <div class="scroll">
-        <table border="1" class="tableAdmin">
-            <tr>
-                <th>Code</th>
-                <th>Score</th>
-                <th>Evaluation Date</th>
-                <th>Comments</th>
-                <th>Employee</th>
-                <th colspan="2">Options</th>
-            </tr>
-            <?php foreach($rating as $renglon) {?>
-            <tr>
-                <td><?= $renglon['id'] ?></td>
-                <td><?= $renglon['score']?></td>
-                <td><?= $renglon['evaluationDate']?></td>
-                <td><?= $renglon['comments']?></td>
-                <td><?= $renglon['employee']?></td>
-                <td><a href="modifiyRating.php?id=<?php echo $renglon['id']?>" class="action-modify">Modify</a></td>
-                <td><a href="deleteRating.php?id=<?php echo $renglon['id']; ?>&action=delete" class="action-delete">Delete</a></td>
-            </tr>
-            <?php }?>
+        <p>In this section you can see the scores you have made on the employees you supervise, you can modify the reports you have made and also delete them.</p>
+        <p>At the bottom you have a form where you can make employee score reports.</p>
+    </div>
+
+    <div class="table-responsive mb-4">
+        <table class="table table-bordered table-striped table-hover table-sm">
+            <thead class="table-dark">
+                <tr>
+                    <th>Code</th>
+                    <th>Score</th>
+                    <th>Evaluation Date</th>
+                    <th>Comments</th>
+                    <th>Employee</th>
+                    <th colspan="2">Options</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($rating as $renglon) { ?>
+                <tr>
+                    <td><?= $renglon['id'] ?></td>
+                    <td><?= $renglon['score']?></td>
+                    <td><?= $renglon['evaluationDate']?></td>
+                    <td><?= $renglon['comments']?></td>
+                    <td><?= $renglon['employee']?></td>
+                    <td><a href="modifiyRating.php?id=<?= $renglon['id']?>" class="btn btn-success btn-sm">Modify</a></td>
+                    <td><a href="deleteRating.php?id=<?= $renglon['id']?>&action=delete" class="btn btn-danger btn-sm">Delete</a></td>
+                </tr>
+                <?php } ?>
+            </tbody>
         </table>
     </div>
-    <div>
-    <h2>Make a score</h2>
-    <form action="addPerformance.php" class="formPage" method="POST">
-            <fieldset><br>
-            <div class="firstInput">
-                    <label for="score">Score</label>
-                    <input type="number" id="score" name="score" placeholder="Score of the employee">
+
+    <!-- Form to make a score -->
+    <div class="mt-4">
+        <h2>Make a score</h2>
+        <form action="addPerformance.php" method="POST" class="needs-validation" novalidate>
+            <fieldset class="border p-4">
+                <div class="mb-3">
+                    <label for="score" class="form-label">Score</label>
+                    <input type="number" id="score" name="score" class="form-control" placeholder="Score of the employee" required>
+                    <div class="invalid-feedback">
+                        Please enter a valid score.
+                    </div>
                 </div>
-                <br>
-                <div>
-                    <label for="evaluationDate">Evaluation Date</label>
-                    <input type="date" id="evaluationDate" name="evaluationDate" >
+
+                <div class="mb-3">
+                    <label for="evaluationDate" class="form-label">Evaluation Date</label>
+                    <input type="date" id="evaluationDate" name="evaluationDate" class="form-control" required>
                 </div>
-                <br>
-                <div>
-                    <label for="comments">Comentarys</label>
-                    <textarea name="comments" id="comments"></textarea>
+
+                <div class="mb-3">
+                    <label for="comments" class="form-label">Comments</label>
+                    <textarea name="comments" id="comments" class="form-control" required></textarea>
                 </div>
-                <br>
-                <div>
-                    <label for="employee">Select a Employee:</label>
-                    <select name="employee" id="employee" required>
+
+                <div class="mb-3">
+                    <label for="employee" class="form-label">Select a Employee</label>
+                    <select name="employee" id="employee" class="form-select" required>
                         <option value="">-- Employee --</option>
                         <?php 
                             $employees = getInfoEmploy($IDUsuario);
                             foreach ($employees as $renglon) { ?>
-                            <option  value="<?= htmlspecialchars($renglon['code']) ?>"><?= htmlspecialchars($renglon['firstName']." ".$renglon['lastName']." ".$renglon['middleName']) ?></option>
+                            <option value="<?= htmlspecialchars($renglon['code']) ?>"><?= htmlspecialchars($renglon['firstName']." ".$renglon['lastName']." ".$renglon['middleName']) ?></option>
                         <?php } ?>
                     </select>
                 </div>
-                <br>
-                <div>
-                    <button type="submit" name="btnAddPerformance">Make a score</button>
+
+                <div class="mb-3">
+                    <button type="submit" name="btnAddPerformance" class="btn btn-primary w-100">Make a score</button>
                 </div>
             </fieldset>
         </form>
     </div>
 </section>
+
 <?php include "../includes/footer.php" ?>
