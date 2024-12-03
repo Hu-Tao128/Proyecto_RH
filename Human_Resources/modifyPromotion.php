71897c33c01063135fa74cd38e928634f854aa6e
Miguellr01 <0323106022@ut-tijuana.edu.mx>
2024-12-03 13:39:02 -0800

    Corregir FALTAL ERROR

--- Human_Resources/modifyPromotion.php
+++ Human_Resources/modifyPromotion.php
@@ -1,5 +1,5 @@
 <?php
-include "../includes/headerHR.php";
+include "../includes/headerRH.php";
 require_once "../includes/config/MySQL_ConexionDB.php";
 include "../admin/functionsAdmin.php";
 require_once "../functions.php"; 
@@ -20,51 +20,46 @@ foreach($promotion as $row){
 
 ?>
 
-<section>
-    <h2>Modifiy a promotion</h2>
-    <form action="updatePromotion.php" class="formPage" method="post">
+<section class="container py-5">
+    <h2 class="text-center mb-4">Modify a Promotion</h2>
+    <form action="updatePromotion.php" method="post" class="bg-light p-4 rounded shadow">
         <fieldset>
-        <div class="firstInput">
-                
-        </div>
-            
-            <div>
-                <label for="code" >Code: <?php echo $row['code']?></label>
-                <input class="inputID" type="text" id="code" name="code" value=<?php echo $row['code']?> readonly>
+            <input type="hidden" id="code" name="code" value="<?php echo $row['code']?>" readonly>
+
+            <div class="mb-3">
+                <label for="code" class="form-label">Promotion Code</label>
+                <input type="text" id="code" name="code" class="form-control" value="<?php echo $row['code']?>" readonly>
             </div>
-            <div>
-                <label for="publicationDate">Publication date: <?php echo $row['publicationDate']?> </label>
+
+            <div class="mb-3">
+                <label for="publicationDate" class="form-label">Publication Date</label>
+                <input type="text" id="publicationDate" class="form-control" value="<?php echo $row['publicationDate']?>" readonly>
             </div>
-            <br>
-            <div>
-                <label for="name">Name of the promotion</label>
-                <input type="text" id="name" name="name" value="<?php echo $row['name']?>" required placeholder="Write the name of the promotion">
+
+            <div class="mb-3">
+                <label for="name" class="form-label">Name of the Promotion</label>
+                <input type="text" id="name" name="name" class="form-control" value="<?php echo $row['name']?>" placeholder="Write the name of the promotion" required>
             </div>
-            <br>
-            <div>
-                <label for="description">Description</label>
-                <textarea name="description" id="description"><?php echo $row['description']?></textarea>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea id="description" name="description" class="form-control" rows="4" style="resize: none;" placeholder="Write the description"><?php echo $row['description']?></textarea>
             </div>
-            <br>
-            <div>
-                <label for="status">Status</label>
-                <select name="status" id="status">
-                    <option value="Active">Active</option>
-                    <option value="Inactive">Inactive</option>
+
+            <div class="mb-3">
+                <label for="status" class="form-label">Status</label>
+                <select name="status" id="status" class="form-select">
+                    <option value="Active" <?php echo ($row['status'] == 'Active' ? 'selected' : ''); ?>>Active</option>
+                    <option value="Inactive" <?php echo ($row['status'] == 'Inactive' ? 'selected' : ''); ?>>Inactive</option>
                 </select>
             </div>
-            <br>
-            
-            <div>
-                <button type="submit" name="btnReport">Update</button>
+
+            <div class="text-center">
+                <button type="submit" name="btnReport" class="btn btn-primary px-4">Update</button>
+                <a href="promotions.php" class="btn btn-secondary px-4 ms-2">Cancel</a>
             </div>
         </fieldset>
     </form>
-    <center>
-    <a href="promotions.php"><button class="buttonCancel">Cancel</button></a>
-    </center>
-    
-    
 </section>
 
 <?php include "../includes/footer.php" ?>
